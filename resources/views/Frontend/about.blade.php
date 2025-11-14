3b3bc2816b9d1f640be5c9a7a008373aadec9e07
Nextus Marketing <marketingteamnextus@gmail.com>
2025-11-15 02:32:21 +0530

    changes done

--- resources/views/Frontend/about.blade.php
+++ resources/views/Frontend/about.blade.php
@@ -1,6 +1,333 @@
 
 @extends('layouts.frontend')
-@section('title') Page Name | Website Name @endsection
+@section('title') About US | Home Defender @endsection
 @section('content')
+<div class="page-header parallaxie">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-12">
+					<div class="page-header-box">
+						<h1 class="wow fadeInUp" data-cursor="-opaque">About us</h1>
+						<nav class="wow fadeInUp" data-wow-delay="0.2s">
+                            <ol class="breadcrumb">
+								<li class="breadcrumb-item"><a href="/">home</a></li>
+								<li class="breadcrumb-item active" aria-current="page">about us</li>
+							</ol>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    <div class="our-commitment">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div class="our-commitment-images">
+                        <div class="our-commitment-img-box">
+                            <div class="commitment-image-1">
+                                <figure class="image-anime reveal">
+                                    <img src="/frontend/images/commitment-image-1.jpg" alt="">
+                                </figure>
+                            </div>
+                            <div class="satisfy-client-box">
+                                <div class="satisfy-client-content">
+                                    <h2><span class="counter">3.5</span>K</h2>
+                                    <p>Our Trusted Clients</p>
+                                </div>
+                                <div class="satisfy-client-images">
+                                    <div class="satisfy-client-image">
+                                        <figure class="image-anime reveal">
+                                            <img src="/frontend/images/satisfy-client-img-1.jpg" alt="">
+                                        </figure>
+                                    </div>
+                                    <div class="satisfy-client-image">
+                                        <figure class="image-anime reveal">
+                                            <img src="/frontend/images/satisfy-client-img-2.jpg" alt="">
+                                        </figure>
+                                    </div>
+                                    <div class="satisfy-client-image">
+                                        <figure class="image-anime reveal">
+                                            <img src="/frontend/images/satisfy-client-img-3.jpg" alt="">
+                                        </figure>
+                                    </div>
+                                    <div class="satisfy-client-image">
+                                        <figure class="image-anime reveal">
+                                            <img src="/frontend/images/satisfy-client-img-4.jpg" alt="">
+                                        </figure>
+                                    </div>
+                                    <div class="satisfy-client-image">
+                                        <figure class="image-anime reveal">
+                                            <img src="/frontend/images/satisfy-client-img-5.jpg" alt="">
+                                        </figure>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="our-commitment-img-box">
+                            <div class="commitment-image-2">
+                                <figure class="image-anime reveal">
+                                    <img src="/frontend/images/commitment-image-2.jpg" alt="">
+                                </figure>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-lg-6">
+                    <div class="our-commitment-content">
+                        <div class="section-title">
+                            <h3 class="wow fadeInUp">About Us</h3>
+                            <h2 class="wow fadeInUp" data-wow-delay="0.2s" data-cursor="-opaque"><span>15 Years of Excellence </span> in Security Solutions</h2>
+                            <p class="wow fadeInUp" data-wow-delay="0.4s">Home Defender is one of the top 100 security companies in the country by SDM Magazine.
+                            </br>
+                            With over 15 years of expertise in the field, we have been committed to delivering the best in security and automation solutions. Home Defender has consistently ranked among the top 100 security companies in the nation by SDM Magazine, a testament to our dedication to quality and innovation.
+                            </br>
+                            </br>
+                            We focus on providing tailored solutions that meet the unique needs of our customers, offering a perfect blend of advanced technology and user-friendly designs. Whether you need to secure your home, business, or industrial property, we’re here to make your life safer and simpler.</p>
+                        </div>
+                    </div>
+                    <div class="commitment-list wow fadeInUp" data-wow-delay="0.6s">
+                        <ul>
+                            <li>Arm/Disarm your home security system</li>
+                            <li>Live Video monitoring</li>
+                            <li>Access from anywhere</li>
+                            <li>View security alerts / notification</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Our Commitment Section End -->
+
+    <div class="our-feature dark-section">
+    <div class="container">
+        <div class="row section-row align-items-center">
+            <div class="col-lg-6 col-md-8">
+                <div class="section-title">
+                    <h3 class="wow fadeInUp">Why Choose Us</h3>
+                    <h2 class="wow fadeInUp" data-wow-delay="0.2s" data-cursor="-opaque">
+                        <span>Home Defender </span>
+                    </h2>
+                    <p>Choose us for expert solutions, reliable service, and unmatched dedication to your security and convenience needs. Your safety is our priority.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-4">
+                <div class="contact-now-circle">
+                    <a href="/contact"><img src="/frontend/images/contact-now-circle.svg" alt=""></a>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="our-feature-box">
+
+                    <div class="feature-item wow fadeInUp">
+                        <div class="icon-box">
+                            <img src="/frontend/images/icon-feature-item-1.svg" alt="">
+                        </div>
+                        <div class="feature-item-content">
+                            <h3>Easy to set up and use (professional installation available)</h3>
+                        </div>
+                    </div>
+
+                    <div class="feature-item wow fadeInUp" data-wow-delay="0.2s">
+                        <div class="icon-box">
+                            <img src="/frontend/images/icon-feature-item-2.svg" alt="">
+                        </div>
+                        <div class="feature-item-content">
+                            <h3>System With Reliable 24/7 Professional Monitoring</h3>
+                        </div>
+                    </div>
+
+                    <div class="feature-item wow fadeInUp" data-wow-delay="0.4s">
+                        <div class="icon-box">
+                            <img src="/frontend/images/icon-feature-item-3.svg" alt="">
+                        </div>
+                        <div class="feature-item-content">
+                            <h3>Ethernet connection with cellular backup so you’re always connected</h3>
+                        </div>
+                    </div>
+
+                    <div class="feature-item wow fadeInUp" data-wow-delay="0.6s">
+                        <div class="icon-box">
+                            <img src="/frontend/images/icon-feature-item-4.svg" alt="">
+                        </div>
+                        <div class="feature-item-content">
+                            <h3>Faster emergency dispatch and fewer false alarms</h3>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-lg-12">
+                <div class="feature-counter-box">
+
+                    <div class="feature-counter-item">
+                        <h2><span class="counter">220</span>+</h2>
+                        <p>Residential area</p>
+                    </div>
+
+                    <div class="feature-counter-item">
+                        <h2><span class="counter">30</span>+</h2>
+                        <p>Malls & Building</p>
+                    </div>
+
+                    <div class="feature-counter-item">
+                        <h2><span class="counter">100</span>+</h2>
+                        <p>Commercial Space</p>
+                    </div>
+
+                    <div class="feature-counter-item">
+                        <h2><span class="counter">700</span>+</h2>
+                        <p>Project Complete</p>
+                    </div>
+
+                    <div class="feature-counter-item">
+                        <h2><span class="counter">25</span>+</h2>
+                        <p>Year of Experience</p>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="about-us">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-6">
+                        <div class="about-us-/frontend/images">
+                            <div class="about-img-1">
+                                <figure class="image-anime reveal">
+                                    <img src="/frontend/images/about-img-1.jpg" alt="">
+                                </figure>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6">
+                        <div class="about-us-content">
+                            <div class="section-title">
+                                <h3 class="wow fadeInUp">We Provide</h3>
+                                <h2 class="wow fadeInUp" data-wow-delay="0.2s" data-cursor="-opaque"><span>Secure Yourself with the Most-Trusted</span>, Easy-To-Use Systems on the Market</h2>
+                                <p class="wow fadeInUp" data-wow-delay="0.4s">The integrity of your home or business is of utmost importance. Highly trained operators rapidly handle fire, burglary, hold-up, low or high temperature, or medical emergency alarm signals. We 24/7/365 Central Station that is both UL listed and New York City Fire Department approved. Transmission technologies include digital, 900 mhz radio, cellular radio, and internet communication.
+                                </p>
+                            </div>
+                            </div>
+                            <div class="about-us-btn" style="margin-top:20px;">
+                                <a href="tel:1-888-447-6320" class="btn-default">Call Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+ <div class="our-feature dark-section">
+        <div class="container">
+            
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="our-feature-box">
+                        <div class="feature-item wow fadeInUp">
+                            <div class="icon-box">
+                                <img src="/frontend/my-img/client-01.png" alt="">
+                            </div>
+                        </div>
+                        <div class="feature-item wow fadeInUp" data-wow-delay="0.2s">
+                            <div class="icon-box">
+                                <img src="/frontend/my-img/client-03.webp" alt="">
+                            </div>
+                        </div>
+                        <div class="feature-item wow fadeInUp" data-wow-delay="0.4s">
+                            <div class="icon-box">
+                                <img src="/frontend/my-img/client-04.webp" alt="">
+                            </div>
+                        </div>
+                        <div class="feature-item wow fadeInUp" data-wow-delay="0.6s">
+                            <div class="icon-box">
+                                <img src="/frontend/my-img/client-05.png" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="page-team">
+    <div class="container">
+
+        <!-- Section Title -->
+        <div class="row justify-content-center">
+            <div class="col-lg-8 text-center">
+                <div class="section-title">
+                    <h2 class="wow fadeInUp" data-wow-delay="0.2s" data-cursor="-opaque">
+                        <span>Expert Security </span>Team
+                    </h2>
+                </div>
+            </div>
+        </div>
+
+        <!-- Team Members -->
+        <div class="row justify-content-center">
+
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="team-item wow fadeInUp">
+                    <div class="team-image">
+                        <a href="team-single.html" data-cursor-text="View">
+                            <figure class="image-anime">
+                                <img src="/frontend/images/team-1.jpg" alt="">
+                            </figure>
+                        </a>
+                    </div>
+                    <div class="team-content text-center">
+                        <h3><a href="team-single.html">Justin Novak</a></h3>
+                        <p>Co founder</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="team-item wow fadeInUp" data-wow-delay="0.2s">
+                    <div class="team-image">
+                        <a href="team-single.html" data-cursor-text="View">
+                            <figure class="image-anime">
+                                <img src="/frontend/images/team-2.jpg" alt="">
+                            </figure>
+                        </a>
+                    </div>
+                    <div class="team-content text-center">
+                        <h3><a href="team-single.html">Norton Berry</a></h3>
+                        <p>Managing Director</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="team-item wow fadeInUp" data-wow-delay="0.4s">
+                    <div class="team-image">
+                        <a href="team-single.html" data-cursor-text="View">
+                            <figure class="image-anime">
+                                <img src="/frontend/images/team-3.jpg" alt="">
+                            </figure>
+                        </a>
+                    </div>
+                    <div class="team-content text-center">
+                        <h3><a href="team-single.html">Edward Smith</a></h3>
+                        <p>Investment Expert</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</div>
 
 @endsection
--- resources/views/Frontend/contact.blade.php
+++ resources/views/Frontend/contact.blade.php
@@ -1,5 +1,138 @@
 @extends('layouts.frontend')
-@section('title') Page Name | Website Name @endsection
+@section('title') Contact Us | Home Defender @endsection
 @section('content')
+<!-- Page Header Start -->
+	<div class="page-header parallaxie">
+		<div class="container">
+			<div class="row align-items-center">
+				<div class="col-lg-12">
+					<div class="page-header-box">
+						<h1 class="wow fadeInUp" data-cursor="-opaque">Contact us</h1>
+						<nav class="wow fadeInUp" data-wow-delay="0.2s">
+                            <ol class="breadcrumb">
+								<li class="breadcrumb-item"><a href="/">home</a></li>
+								<li class="breadcrumb-item active" aria-current="page">contact us</li>
+							</ol>
+						</nav>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    <div class="page-contact-us">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-4">
+                    <div class="contact-us-content">
+                        <div class="section-title">
+                            <h3 class="wow fadeInUp">contact us</h3>
+                            <h2 class="wow fadeInUp" data-wow-delay="0.2s" data-cursor="-opaque"><span>Secure your</span> with us</h2>
+                            <p class="wow fadeInUp" data-wow-delay="0.4s">Have questions or need a customized security solution? Our team is here to assist you. Get in touch with us today.</p>
+                        </div>
+                        <div class="contact-social-list wow fadeInUp" data-wow-delay="0.6s">
+                            <ul>
+                                <li><a href="#"><i class="fa-brands fa-instagram"></i></a></li>
+                                <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
+                                <li><a href="#"><i class="fa-brands fa-pinterest-p"></i></a></li>
+                                <li><a href="#"><i class="fa-brands fa-x-twitter"></i></a></li>
+                            </ul>
+                        </div>
 
+                    </div>
+                </div>
+
+                <div class="col-lg-8">
+                    <div class="contact-info-list">
+                        <div class="contact-info-item wow fadeInUp">
+                            <div class="icon-box">
+                                <img src="/frontend/images/icon-phone.svg" alt="">
+                            </div>
+                            <div class="contact-item-content">
+                                <p>phone number</p>
+                                <h3><a href="tel:+456789254">+(1) 456 789 254</a></h3>
+                            </div>
+                        </div>
+                        <div class="contact-info-item wow fadeInUp" data-wow-delay="0.2s">
+                            <div class="icon-box">
+                                <img src="/frontend/images/icon-mail.svg" alt="">
+                            </div>
+                            <div class="contact-item-content">
+                                <p>email address</p>
+                                <h3><a href="mailto:info@homedefender.net">info@homedefender.net</a></h3>
+                            </div>
+                        </div>
+                        <div class="contact-info-item location-item wow fadeInUp" data-wow-delay="0.4s">
+                            <div class="icon-box">
+                                <img src="/frontend/images/icon-location.svg" alt="">
+                            </div>
+                            <div class="contact-item-content">
+                                <p>location</p>
+                                <h3>9620 Hillock ct, Burke VA, 22015</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Page Contact Us End -->
+
+      <!-- Contact Us Form Section Start -->
+    <div class="contact-form-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="contact-us-box">
+                        <!-- Google Map IFrame Start -->
+                        <div class="google-map-iframe">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d96737.10562045308!2d-74.08535042841811!3d40.739265258395164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c24fa5d33f083b%3A0xc80b8f06e177fe62!2sNew%20York%2C%20NY%2C%20USA!5e0!3m2!1sen!2sin!4v1703158537552!5m2!1sen!2sin" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                        </div>
+                        <!-- Google Map IFrame End -->
+
+                        <!-- Contact Us Form Start -->
+                        <div class="contact-us-form">
+                            <div class="section-title">
+                                <h2 class="wow fadeInUp" data-wow-delay="0.2s" data-cursor="-opaque">Get in Touch</h2>
+                                <p class="wow fadeInUp" data-wow-delay="0.4s">We specialize in providing top-quality security and CCTV solutions to safeguard your home and business. </p>
+                            </div>
+                            <form id="contactForm" action="#" method="POST" data-toggle="validator" class="contact-form wow fadeInUp" data-wow-delay="0.6s">
+                                <div class="row">
+                                    <div class="form-group col-md-6 mb-4">
+                                        <input type="text" name="fname" class="form-control" id="fname" placeholder="First name" required>
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+
+                                    <div class="form-group col-md-6 mb-4">
+                                        <input type="text" name="lname" class="form-control" id="lname" placeholder="Last name" required>
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+
+                                    <div class="form-group col-md-6 mb-4">
+                                        <input type="email" name ="email" class="form-control" id="email" placeholder="E-mail" required>
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+
+                                    <div class="form-group col-md-6 mb-4">
+                                        <input type="text" name="phone" class="form-control" id="phone" placeholder="Phone no." required>
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+
+                                    <div class="form-group col-md-12 mb-5">
+                                        <textarea name="message" class="form-control" id="message" rows="2" placeholder="Message"></textarea>
+                                        <div class="help-block with-errors"></div>
+                                    </div>
+
+                                    <div class="col-md-12">
+                                        <button type="submit" class="btn-default"><span>send message</span></button>
+                                        <div id="msgSubmit" class="h3 hidden"></div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Contact Us Form Section End -->
 @endsection
