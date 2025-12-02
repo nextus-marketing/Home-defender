dbf9a86427c91d167d9f533a6bba268c6cad9194
Nextus Marketing <marketingteamnextus@gmail.com>
2025-12-03 02:32:20 +0530

    changes done

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -93,6 +93,11 @@
 </script>
 </head>
 <body>
+<a href="tel:1-888-447-6320" class="call-now-float" aria-label="Call Now">
+    <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" width="22" height="22">
+        <path d="M6.62 10.79a15.053 15.053 0 006.59 6.59l2.2-2.2a1 1 0 011.11-.21 11.36 11.36 0 003.54.57 1 1 0 011 1V20a1 1 0 01-1 1A17 17 0 013 5a1 1 0 011-1h3.5a1 1 0 011 1 11.36 11.36 0 00.57 3.54 1 1 0 01-.45 1.25l-2.2 2.2z"/>
+    </svg>
+</a>
 
 
     <!-- Header Start -->
@@ -175,10 +180,6 @@
     };
 </script>
 
-
-<a href="tel:+18884476320" class="call-now-btn">
-    <i class="fa fa-phone"></i>
-</a>
 
 <button id="scrollTopBtn">
     <i class="fa fa-arrow-up"></i>
