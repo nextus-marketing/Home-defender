38ad4423cdd90c82e7326ed722e9430f1eff2eff
Nextus Marketing <marketingteamnextus@gmail.com>
2025-12-05 22:57:55 +0530

    changes done

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -4,6 +4,11 @@
     <!-- Google tag (gtag.js) --> <script async src="https://www.googletagmanager.com/gtag/js?id=G-98T3F9KPCE"></script> <script>   window.dataLayer = window.dataLayer || [];   function gtag(){dataLayer.push(arguments);}   gtag('js', new Date());   gtag('config', 'G-98T3F9KPCE'); </script>
     <title>@yield(section: 'title')</title>
 	<!-- Meta -->
+@if(View::hasSection('meta'))
+    @yield('meta')
+@elseif(View::hasSection('meta_description'))
+    <meta name="description" content="@yield('meta_description')">
+@endif
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
@@ -35,11 +40,7 @@
     <!-- Mouse Cursor Css File -->
 	<link rel="stylesheet" href="/frontend/css/my.css">
 
-     @if(View::hasSection('meta'))
-    @yield('meta')
-    @elseif(View::hasSection('meta_description'))
-        <meta name="description" content="@yield('meta_description')">
-    @endif
+    
     @yield('structured_data')
     <script type="application/ld+json">
 {
